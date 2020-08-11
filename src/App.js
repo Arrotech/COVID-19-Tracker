82d5d9567579be2b123022b70294094eaa450399
Arrotech <arrotechdesign@gmail.com>
2020-08-11 16:12:41 +0300

    Feature (Display Table Data)
    What does this pr do?
    
    Display Table Data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
+import {sortData} from './util';
 
 function App() {
 	const [countries, setCountries] = useState([]);
 	const [country, setCountry] = useState('worldwide');
-	const [countryInfo, setCountryInfo] = useState({});
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
 	useEffect(() => {
 		fetch('https://disease.sh/v3/covid-19/all')
@@ -25,7 +28,9 @@ function App() {
 					const countries = data.map((country) => ({
 						name: country.country,
 						value: country.countryInfo.iso2,
-					}));
+          }));
+          const sortedData = sortData(data);
+					setTableData(sortedData);
 					setCountries(countries);
 				});
 		};
@@ -74,7 +79,8 @@ function App() {
 			</div>
 			<Card className="app__right">
 				<CardContent>
-					<h3>Live Cases by Country</h3>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData}></Table>
 					<h3>Worldwide New Cases</h3>
 				</CardContent>
 			</Card>
