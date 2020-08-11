7a895615c0335833f42545907aebe6569a8f1f8e
Arrotech <arrotechdesign@gmail.com>
2020-08-11 20:03:05 +0300

    display map

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/c
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
-import {sortData} from './util';
+import { sortData, prettyPrintStat } from './util';
 import LineGraph from './LineGraph';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
 	const [countries, setCountries] = useState([]);
 	const [country, setCountry] = useState('worldwide');
-  const [countryInfo, setCountryInfo] = useState({});
-  const [tableData, setTableData] = useState([]);
+	const [countryInfo, setCountryInfo] = useState({});
+	const [tableData, setTableData] = useState([]);
+	const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+	const [mapZoom, setMapZoom] = useState(3);
+	const [mapCountries, setMapCountries] = useState([]);
+	const [casesType, setCasesType] = useState('cases');
 
 	useEffect(() => {
 		fetch('https://disease.sh/v3/covid-19/all')
@@ -29,9 +34,10 @@ function App() {
 					const countries = data.map((country) => ({
 						name: country.country,
 						value: country.countryInfo.iso2,
-          }));
-          const sortedData = sortData(data);
+					}));
+					const sortedData = sortData(data);
 					setTableData(sortedData);
+					setMapCountries(data);
 					setCountries(countries);
 				});
 		};
@@ -52,6 +58,8 @@ function App() {
 			.then((data) => {
 				setCountry(countryCode);
 				setCountryInfo(data);
+				setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+				setMapZoom(4);
 			});
 	};
 
@@ -72,18 +80,38 @@ function App() {
 					</FormControl>
 				</div>
 				<div className="app__stats">
-					<InfoBox title="Coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
-					<InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
-					<InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+					<InfoBox
+						isRed
+						active={casesType === 'cases'}
+						onClick={(e) => setCasesType('cases')}
+						title="Coronavirus cases"
+						cases={prettyPrintStat(countryInfo.todayCases)}
+						total={prettyPrintStat(countryInfo.cases)}
+					/>
+					<InfoBox
+						active={casesType === 'recovered'}
+						onClick={(e) => setCasesType('recovered')}
+						title="Recovered"
+						cases={prettyPrintStat(countryInfo.todayRecovered)}
+						total={prettyPrintStat(countryInfo.recovered)}
+					/>
+					<InfoBox
+						isRed
+						active={casesType === 'deaths'}
+						onClick={(e) => setCasesType('deaths')}
+						title="Deaths"
+						cases={prettyPrintStat(countryInfo.todayDeaths)}
+						total={prettyPrintStat(countryInfo.deaths)}
+					/>
 				</div>
-				<Map />
+				<Map casesType={casesType} countries={mapCountries} center={mapCenter} zoom={mapZoom} />
 			</div>
 			<Card className="app__right">
 				<CardContent>
-          <h3>Live Cases by Country</h3>
-          <Table countries={tableData}></Table>
-          <h3>Worldwide New Cases</h3>
-          <LineGraph/>
+					<h3>Live Cases by Country</h3>
+					<Table countries={tableData}></Table>
+					<h3 className="app__graphTitle">Worldwide New Cases {casesType}</h3>
+					<LineGraph className="app__graph" casesType={casesType} />
 				</CardContent>
 			</Card>
 		</div>
--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,13 +1,19 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
 import React from 'react';
+import './InfoBox.css';
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
 	return (
-		<Card className="infoBox">
+		<Card onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'}`}>
 			<CardContent>
-				<Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-				<h2 className="infoBox__cases">{cases}</h2>
-				<Typography className="infoBox__total" color="textSecondary">{total} Total</Typography>
+				<Typography className="infoBox__title" color="textSecondary">
+					{title}
+				</Typography>
+				<h2 className={`infoBox__cases ${!isRed && 'infoBox__cases--green'}`}> {cases} </h2>
+				<Typography className="infoBox__total" color="textSecondary">
+					{total}
+					Total
+				</Typography>
 			</CardContent>
 		</Card>
 	);
